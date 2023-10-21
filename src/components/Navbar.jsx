8c8012740d88e16f1f8e37e0a9176d653932ea64
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-10-21 06:57:50 -0300

    Estilos da navbar 29:00 of the video https://www.youtube.com/watch?v=QwarZBtFoFA

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import logo from "../assets/pizzaLogo.png"
 import { Link } from 'react-router-dom'
+import "../styles/navbar.css"
 
 function Navbar() {
   return (
     <div className='navbar'>
-      <div className='leftside'>
+      <div className='leftSide'>
         <img src={logo} alt="Logo" />
       </div>
-      <div className='rightside'>
+      <div className='rightSide'>
         <Link to="/"> Home</Link>
         <Link to="/menu"> Menu</Link>
         <Link to="/about"> About</Link>
