58d337454a1e2dbbd77fb28d57efa750d9ad973e
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-10-24 15:52:05 -0300

    Background image da home 47:00 https://www.youtube.com/watch?v=QwarZBtFoFA

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import BannerImage from '../assets/pizza.jpeg'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div 
+        className="headerContainer" 
+        style={{ backgroundImage: `url(${BannerImage})` }}>
+        <h1>Pedro's Pizzeria</h1>  
+        <p>PIZZA TO FIT ANY TASTE</p>
+        <Link to="/menu">
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
     </div>
   )
 }
