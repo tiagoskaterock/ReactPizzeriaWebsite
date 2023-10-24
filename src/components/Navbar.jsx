9db80802c28dd9ebf5ab93e415acb120c48de1fd
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-10-24 09:57:05 -0300

    Hamburger button on navbar 40:40 of the video https://www.youtube.com/watch?v=QwarZBtFoFA

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/pizzaLogo.png"
 import { Link } from 'react-router-dom'
+import ReorderIcon from '@mui/icons-material/Reorder';
 import "../styles/navbar.css"
 
 function Navbar() {
+
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks( ! openLinks)
+  }
+
   return (
     <div className='navbar'>
-      <div className='leftSide'>
+      <div className='leftSide' id={openLinks ? 'open' : 'close'}>
         <img src={logo} alt="Logo" />
+        <div className="hiddenLinks">
+          <Link to="/"> Home</Link>
+          <Link to="/menu"> Menu</Link>
+          <Link to="/about"> About</Link>
+          <Link to="/content"> Content</Link>
+        </div>
       </div>
       <div className='rightSide'>
         <Link to="/"> Home</Link>
         <Link to="/menu"> Menu</Link>
         <Link to="/about"> About</Link>
         <Link to="/content"> Content</Link>
+
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
+        
       </div>
     </div>
   )
