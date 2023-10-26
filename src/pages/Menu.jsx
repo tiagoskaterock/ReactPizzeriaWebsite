058e9521b3417a0b9727909a442f5c30ae51af1c
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-10-26 14:27:37 -0300

    Menu page at 1:11:30 of the video https://www.youtube.com/watch?v=QwarZBtFoFA

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { MenuList } from "../helpers/MenuList"
+import MenuItem from "../components/MenuItem"
+import "../styles/menu.css"
 
 function Menu() {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="menu">
+      <h1 className="menuTitle">Our Menu</h1>
+      <div className="menuList">
+        {
+          MenuList.map((menuItem, key) => {
+            return <MenuItem 
+                key={key}
+                image={menuItem.image} 
+                name={menuItem.name} 
+                price={menuItem.price}
+              />
+        })}
+      </div>
     </div>
   )
 }
